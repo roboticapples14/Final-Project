f2836eb00cc862daf259c3d31b8134c4d72b0af5
roboticapples14 <36723342+roboticapples14@users.noreply.github.com>
2018-07-24 15:52:53 -0700

    Reformatted
    Added actual questions and reformatted

--- quiz.js
+++ quiz.js
@@ -1,21 +1,5 @@
-var quiz = {}
+function submit() {
 
- quiz.view = function(state, props) {
-
-  state.$looks = state.$smarts = {
-    _onclick: function(event) {
-      props.totals[this.name] = parseInt(this.value)
-    }
-  }
-
-  state.results = props.totals.smarts + props.totals.looks
 }
-
-var props = {
-  totals: {
-    smarts: 0,
-    looks: 0
-  }
-}
-
-mag.module("quiz", quiz, props)
+var x = document.createElement("radiodemo2");
+x.setAttribute("type", "radio");
